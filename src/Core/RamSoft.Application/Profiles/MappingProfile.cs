3324e76aa132cdd9440fe4e90f2a5a5a252445ae
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2024-09-12 23:35:36 +0330

    add MappingProfile

---
+++ src/Core/RamSoft.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RamSoft.Application.Features.StatesFeature.Commands.Create;
+using RamSoft.Domain.Jira;
+
+namespace RamSoft.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateStatesCommand, States>().ReverseMap();
+        }
+    }
+}
