61a00b68900e2da39506ff88ae32725ef64311c5
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2024-09-12 19:40:25 +0330

    Add Base classes in Domain

---
+++ src/Core/RamSoft.Domain/Base/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace RamSoft.Domain.Base
+{
+    public abstract class BaseEntity<T>
+    {
+
+        public required T Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public required string CreatorID { get; set; }
+        public DateTime LastEditDate { get; set; }
+        public string? LastEditorID { get; set; }
+    }
+
+    public abstract class BaseEntity : BaseEntity<int>
+    {
+    }
+}
---
+++ src/Core/RamSoft.Domain/Base/EntityLog.cs
@@ -0,0 +1,9 @@
+﻿namespace RamSoft.Domain.Base
+{
+    public class EntityLog : BaseEntity
+    {
+        public required string ClassName { get; set; }
+        public required string ChangeType { get; set; }
+        public required string Key { get; set; }
+    }
+}
