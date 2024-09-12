649dc60dc09e7be709d7987f09c69dc6802ca268
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2024-09-12 23:35:54 +0330

    add ApplicationServiceRegistration

---
+++ src/Core/RamSoft.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace RamSoft.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+
+            return services;
+        }
+    }
+}
