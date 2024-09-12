db491478ed99c99ccd83b86f05103ab685115dd8
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2024-09-13 00:23:49 +0330

    Add cancellationToken

--- src/Core/RamSoft.Application/Contracts/Base/IGenericRepository.cs
+++ src/Core/RamSoft.Application/Contracts/Base/IGenericRepository.cs
@@ -5,19 +5,19 @@ namespace RamSoft.Application.Contracts.Base
 {
     public interface IGenericRepository<T> where T : BaseEntity 
     {
-        Task<IReadOnlyList<T>> GetAll();
-        Task<T> Get(int id);
-        Task<IReadOnlyList<T>> Get(Expression<Func<T, bool>> predicate);
-        Task<List<T>> Get(List<int> ids);
-        Task<(IQueryable<T>, int)> Get(string Filter, string Order, int? PageNumber, int? PageSize, bool? disableTracking = true);
-        Task<T> Add(T entity);
-        Task<bool> Exists(int id);
-        Task Update(T entity);
-        Task Delete(T entity);
-        Task Delete(int id);
-        Task Delete(List<int> ids);
-        T AddEntity(T entity);
-        List<T> AddRangeEntity(List<T> list);
-        Task<List<T>> AddRangeEntityAsync(List<T> list);
+        Task<IReadOnlyList<T>> GetAll(CancellationToken cancellationToken);
+        Task<T> Get(int id, CancellationToken cancellationToken);
+        Task<IReadOnlyList<T>> Get(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken);
+        Task<List<T>> Get(List<int> ids, CancellationToken cancellationToken);
+        Task<(IQueryable<T>, int)> Get(string Filter, string Order, int? PageNumber, int? PageSize, CancellationToken cancellationToken, bool? disableTracking = true);
+        Task<T> Add(T entity, CancellationToken cancellationToken);
+        Task<bool> Exists(int id, CancellationToken cancellationToken);
+        Task Update(T entity, CancellationToken cancellationToken);
+        Task Delete(T entity, CancellationToken cancellationToken);
+        Task Delete(int id, CancellationToken cancellationToken);
+        Task Delete(List<int> ids, CancellationToken cancellationToken);
+        T AddEntity(T entity, CancellationToken cancellationToken);
+        List<T> AddRangeEntity(List<T> list, CancellationToken cancellationToken);
+        Task<List<T>> AddRangeEntityAsync(List<T> list, CancellationToken cancellationToken);
     }
 }
