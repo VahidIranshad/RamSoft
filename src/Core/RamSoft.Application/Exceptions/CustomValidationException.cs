b2c9b39b2f4395ed848ee36ba6a854fce6d0abe1
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2024-09-12 23:34:22 +0330

    add exception method

---
+++ src/Core/RamSoft.Application/Exceptions/CustomValidationException.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+
+namespace RamSoft.Application.Exceptions
+{
+    public class CustomValidationException : ApplicationException
+    {
+        public List<string> Errors { get; set; } = new List<string>();
+
+        public CustomValidationException(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Errors.Add(error.ErrorMessage);
+            }
+        }
+    }
+}
