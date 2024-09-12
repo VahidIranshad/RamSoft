af26462d80df1c3ae6c81a3c9dfa794c56d3e0db
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2024-09-12 23:34:57 +0330

    add base for command and query

---
+++ src/Core/RamSoft.Application/Features/Base/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace RamSoft.Application.Features.Base
+{
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
